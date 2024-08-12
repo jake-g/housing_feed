533c0c5573244853736884f86e03b87a285b12ca
jake-g <omonoid@gmail.com>
2024-08-12 02:21:26 -0700

    update site

--- run_home_scraper_and_push_site.ps1
+++ run_home_scraper_and_push_site.ps1
@@ -1,6 +1,5 @@
 $Host.UI.RawUI.WindowTitle = "home-scraper"
 Set-Location "D:\Projects\_Projects_Synced\home-scraper"
 python generate_html_from_housing_emails.py | Tee-Object -FilePath "generate_html_from_housing_emails.log"
-git commit -am "update site"
-#git push
+git commit -am "update site"; git push
 pause
